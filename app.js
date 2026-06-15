a667c96c4b604bf5e50cb8d4e2d3878f11f53d27
bhavy-art <bhavybatra085@gmail.com>
2026-06-15 13:00:14 +0530

    use of middle-ware

--- app.js
+++ app.js
@@ -1,10 +1,63 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res)=>{
-    console.log("hii i am middleware");
-    res.send("middleware finished");
-});
+// app.use((req, res, next)=>{
+//     console.log("hii i am 1st middleware");
+//     // res.send("middleware finished");
+//     next();
+// });
+
+// app.use((req, res, next)=>{
+//     console.log("hii i am 2nd middleware");
+//     next();
+// });.
+
+
+
+// logger -> use to log information
+// app.use((req, res, next)=>{
+//     console.log(req.method);
+//     console.log(req.hostname);
+//     console.log(req.path);
+//     req.setTime = new Date(Date.now()).toString();
+//     console.log(req.time);
+//     next();
+// });
+
+
+// app.use("/random",(req,res,next)=>{
+//     console.log("i am only for random");
+//     next();
+// });
+
+// for giving access
+// app.use("/api",(req, res, next)=>{
+//     let {token} = req.query;
+//     if(token === "giveaccess"){
+//         next();
+//     }else{
+//         res.send("ACCESS DENIED");
+//     }
+// })
+
+// app.get("/api",(req,res)=>{
+//     res.send("data");
+// })
+
+
+// from this we can check multiple middleware and give access
+const checkToken = (req, res, next)=>{
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }else{
+        res.send("ACCESS DENIED");
+    }
+}
+
+app.get("/api", checkToken,(req,res)=>{
+    res.send("data");
+})
 
 app.get("/",(req,res)=>{
     res.send("hii i am root");
@@ -13,6 +66,11 @@ app.get("/",(req,res)=>{
 app.get("/random",(req,res)=>{
     res.send("this is a random page");
 });
+
+// 404 error
+app.use((req,res)=>{
+    res.send("page not found");
+})
 
 app.listen(8080,()=>{
     console.log("Listen to port 8080");
